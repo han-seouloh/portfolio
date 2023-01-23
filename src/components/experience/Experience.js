c00eec65ebdbaf4a2b63b3448607d2bf91f563ec
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-01-23 17:31:41 -0600

    Refactor component and add hover feature

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,17 +1,34 @@
-import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectPlanets, setPlanet } from '../../store/planetsSlice';
+import { selectPlanets, setPlanetAnimation, setPlanetOrbit } from '../../store/planetsSlice';
 import styles from './Experience.module.css';
 
 export const Experience = () => {
   const dispatch = useDispatch();
   const { planets } = useSelector(selectPlanets);
-  const visibility = planets.experience.visibility;
   const animation = planets.experience.animation;
+  const hover = planets.experience.hover;
+
+  const handleHoverOn = () => {
+    dispatch(setPlanetAnimation(false));
+    dispatch(setPlanetOrbit({name:'experience', payload: true}));
+  };
+  const handleHoverOff = () => {
+    dispatch(setPlanetAnimation(true));
+    dispatch(setPlanetOrbit({name:'experience', payload: false}));
+  };
 
   return (
-    <div className={styles['orbit']}>
-      <section className={visibility ? styles['experience'] : styles['hide']}>
+    <div
+      className={`
+        ${styles['orbit']}
+        ${animation ? null : styles['pause']}
+        ${hover ? styles['orbit-hover'] : null}
+      `}
+    >
+      <section id='experience' className={styles['experience']}
+        onMouseEnter = {handleHoverOn}
+        onMouseLeave = {handleHoverOff}
+      >
       </section>
     </div>
   );
