e3b14d89c63585fc44c01d21d137889ec0afc9e7
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-01-23 17:22:29 -0600

    Refactor components in App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import { About } from './components/about/About';
-import { Projects } from './components/projects/Projects';
-import { CurrentProjects } from './components/projects/CurrentProjects';
 
 function App() {
   return (
     <div id='app'>
       <About />
-      <CurrentProjects />
-      <Projects />
     </div>
   );
 }
