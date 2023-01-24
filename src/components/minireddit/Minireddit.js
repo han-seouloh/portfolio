fcdf6666cc76c4d8533e9c8c51ea6cb576dc0f1a
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-01-23 20:03:19 -0600

    Refactor component and add hover feature

--- src/components/minireddit/Minireddit.js
+++ src/components/minireddit/Minireddit.js
@@ -1,17 +1,35 @@
-import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectPlanets, setPlanet } from '../../store/planetsSlice';
+import { selectPlanets, setPlanetAnimation, setPlanetOrbit } from '../../store/planetsSlice';
 import styles from './Minireddit.module.css';
 
 export const Minireddit = () => {
   const dispatch = useDispatch();
   const { planets } = useSelector(selectPlanets);
-  const visibility = planets.minireddit.visibility;
   const animation = planets.minireddit.animation;
+  const hover = planets.minireddit.hover;
+
+  const handleHoverOn = () => {
+    dispatch(setPlanetAnimation(false));
+    dispatch(setPlanetOrbit({name: 'minireddit', payload: true}));
+  };
+  const handleHoverOff = () => {
+    dispatch(setPlanetAnimation(true));
+    dispatch(setPlanetOrbit({name: 'minireddit', payload: false}));
+  };
 
   return (
-    <div className={styles['orbit']}>
-      <section className={visibility ? styles['minireddit'] : styles['hide']}>
+    <div 
+      className={`
+        ${styles['orbit']}
+        ${animation ? null : styles['pause']}
+        ${hover ? styles['orbit-hover'] : null}
+      `}
+    >
+      <section 
+        className={styles['minireddit']}
+        onMouseEnter = {handleHoverOn}
+        onMouseLeave = {handleHoverOff}
+      >
       </section>
     </div>
   );
