990e9ed66709379d4c03d9fb451765d146f2925b
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-01-24 18:42:44 -0600

    Refactor component and add hover feature

--- src/components/tanaka/Tanaka.js
+++ src/components/tanaka/Tanaka.js
@@ -1,17 +1,35 @@
-import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectPlanets, setPlanet } from '../../store/planetsSlice';
+import { selectPlanets, setPlanetAnimation, setPlanetOrbit } from '../../store/planetsSlice';
 import styles from './Tanaka.module.css';
 
 export const Tanaka = () => {
   const dispatch = useDispatch();
   const { planets } = useSelector(selectPlanets);
-  const visibility = planets.tanaka.visibility;
   const animation = planets.tanaka.animation;
+  const hover = planets.tanaka.hover;
+
+  const handleHoverOn = () => {
+    dispatch(setPlanetAnimation(false));
+    dispatch(setPlanetOrbit({name:'tanaka', payload: true}));
+  };
+  const handleHoverOff = () => {
+    dispatch(setPlanetAnimation(true));
+    dispatch(setPlanetOrbit({name:'tanaka', payload: false}));
+  };
 
   return (
-    <div className={styles['orbit']}>
-      <section className={visibility ? styles['tanaka'] : styles['hide']}>
+    <div
+      className={`
+        ${styles['orbit']}
+        ${animation ? null : styles['pause']}
+        ${hover ? styles['orbit-hover'] : null}
+      `}
+    >
+      <section
+        className={styles['tanaka']}
+        onMouseEnter = {handleHoverOn}
+        onMouseLeave = {handleHoverOff}
+      >
       </section>
     </div>
   );
