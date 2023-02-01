c20829f12e7638799d6f1686850b85ec2ff84c74
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-02-01 14:53:54 -0600

    Refactor component and add hover, focus and preview features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import { useSelector } from 'react-redux';
+import { selectPlanets} from './store/planetsSlice';
+
+/*Components*/
 import { About } from './components/about/About';
+import { Preview } from './components/preview/Preview';
+
+/*Styles*/
+import styles from './App.module.css';
 
 function App() {
+  const { planets } = useSelector(selectPlanets);
+  const focus = planets.about.focus;
+
   return (
-    <div id='app'>
-      <About />
+    <div className={styles.app}>
+      <div className={`${focus ? styles['planets-div-focus'] : styles['planets-div']}`}>
+        <About />
+      </div>
+      <div className={`${focus ? styles['info-div-focus'] : styles['info-div']}`}>
+        <Preview />
+      </div>
     </div>
   );
 }
