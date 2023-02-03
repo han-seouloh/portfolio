546794bd64ee46ffb607f63d48ac9d2dbbe4ad1c
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-02-02 20:13:15 -0600

    Refactor component to change based on selection

--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -4,16 +4,16 @@ import { selectPlanets } from '../../store/planetsSlice';
 import styles from './Preview.module.css';
 
 export const Preview = () => {
-  const { planets } = useSelector(selectPlanets);
+  const { planets, selection } = useSelector(selectPlanets);
   const { previews } = useSelector(selectInfo);
-  const heading = previews['about'].heading;
-  const body = previews['about'].body;
-  const focus = planets.about.focus;
+  const heading = previews[selection].heading;
+  const body = previews[selection].body;
+  const focus = planets[selection].focus;
 
   return (
     <aside className={focus ? styles.preview : styles.hidden}>
       <h2 className={styles.heading}>{heading}</h2>
-      <p className={styles.body}>{body}</p>
+      <p className={focus ? styles.body : styles['hidden-body']}>{body}</p>
     </aside>
   );
 };
