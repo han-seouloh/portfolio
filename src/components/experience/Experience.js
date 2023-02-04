89c6ffc698c037e9aa867aa63f98fa5ad7f537eb
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-02-04 12:42:55 -0600

    Add individual click counter functionality and refactor component

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,12 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { selectPlanets, setPlanetAnimation, setPlanetOrbit } from '../../store/planetsSlice';
+import { selectPlanets, setPlanetAnimation, setPlanetFocus, setPlanetOrbit, setPlanetView, setSelection } from '../../store/planetsSlice';
+import { selectClicks, setClickNum } from '../../store/clicksSlice';
 import styles from './Experience.module.css';
 
 export const Experience = () => {
   const dispatch = useDispatch();
   const { planets } = useSelector(selectPlanets);
+  const { clicks } = useSelector(selectClicks);
+  const click = clicks.experience;
   const animation = planets.experience.animation;
   const hover = planets.experience.hover;
+  const focus = planets.experience.focus;
+  const view = planets.experience.view;
 
   const handleHoverOn = () => {
     dispatch(setPlanetAnimation(false));
@@ -16,18 +21,49 @@ export const Experience = () => {
     dispatch(setPlanetAnimation(true));
     dispatch(setPlanetOrbit({name:'experience', payload: false}));
   };
+  const handleClick = () => {
+    switch (click) {
+      case 0:
+        dispatch(setPlanetFocus({name:'experience', payload: true}));
+        dispatch(setPlanetAnimation(true));
+        dispatch(setSelection('experience'))
+        dispatch(setClickNum({name:'experience', clickNum: 1}));
+        break;
+      case 1:
+        dispatch(setPlanetFocus({name:'experience', payload: false}));
+        dispatch(setPlanetView({name:'experience', payload: true}));
+        dispatch(setPlanetAnimation(true));
+        dispatch(setClickNum({name:'experience', clickNum: 2}));
+        break;
+      case 2:
+        dispatch(setPlanetView({name:'experience', payload: false}));
+        dispatch(setPlanetAnimation(true));
+        dispatch(setPlanetOrbit({name:'experience', payload: false}));
+        dispatch(setClickNum({name:'experience', clickNum: 0}));
+        break;
+      default:
+        dispatch(setClickNum({name:'experience', clickNum: 0}));
+        break; 
+    };
+  };
 
   return (
     <div
       className={`
         ${styles['orbit']}
         ${animation ? null : styles['pause']}
-        ${hover ? styles['orbit-hover'] : null}
+        ${hover || focus ? styles['orbit-hover'] : null}
       `}
     >
-      <section id='experience' className={styles['experience']}
+      <section 
+        className={`
+          ${styles['experience']}
+          ${focus ? styles['experience-focus'] : null}
+          ${view ? styles['experience-view'] : null}
+        `}
         onMouseEnter = {handleHoverOn}
         onMouseLeave = {handleHoverOff}
+        onClick = {handleClick}
       >
       </section>
     </div>
