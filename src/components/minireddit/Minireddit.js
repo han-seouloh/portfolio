55086ec2831c7688edf56b51eaf42991e966ca0e
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-02-07 13:31:25 -0600

    Add click handler and update stylesheet

--- src/components/minireddit/Minireddit.js
+++ src/components/minireddit/Minireddit.js
@@ -1,12 +1,19 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { selectPlanets, setPlanetAnimation, setPlanetOrbit } from '../../store/planetsSlice';
+import { selectClicks, setClickNum } from '../../store/clicksSlice';
+import { selectPlanets, setPlanetAnimation, setPlanetFocus, setPlanetOrbit, setPlanetView, setSelection } from '../../store/planetsSlice';
 import styles from './Minireddit.module.css';
 
 export const Minireddit = () => {
   const dispatch = useDispatch();
   const { planets } = useSelector(selectPlanets);
+  const { clicks } = useSelector(selectClicks);
+  const click = clicks.minireddit;
   const animation = planets.minireddit.animation;
   const hover = planets.minireddit.hover;
+  const focus = planets.minireddit.focus;
+  const view = planets.minireddit.view;
+  const tanakaFocus = planets.tanaka.focus;
+  const tanakaView = planets.tanaka.view;
 
   const handleHoverOn = () => {
     dispatch(setPlanetAnimation(false));
@@ -16,19 +23,56 @@ export const Minireddit = () => {
     dispatch(setPlanetAnimation(true));
     dispatch(setPlanetOrbit({name: 'minireddit', payload: false}));
   };
+  const handleClick = () => {
+    switch (click) {
+      case 0:
+        dispatch(setPlanetFocus({name:'minireddit', payload: true}));
+        dispatch(setPlanetAnimation(true));
+        dispatch(setSelection('minireddit'))
+        dispatch(setClickNum({name:'minireddit', clickNum: 1}));
+        break;
+      case 1:
+        dispatch(setPlanetFocus({name:'minireddit', payload: false}));
+        dispatch(setPlanetView({name:'minireddit', payload: true}));
+        dispatch(setPlanetAnimation(true));
+        dispatch(setClickNum({name:'minireddit', clickNum: 2}));
+        break;
+      case 2:
+        dispatch(setPlanetView({name:'minireddit', payload: false}));
+        dispatch(setPlanetAnimation(true));
+        dispatch(setPlanetOrbit({name:'minireddit', payload: false}));
+        dispatch(setClickNum({name:'minireddit', clickNum: 0}));
+        break;
+      default:
+        dispatch(setClickNum({name:'minireddit', clickNum: 0}));
+        break; 
+    };
+  };
 
   return (
     <div 
       className={`
         ${styles['orbit']}
         ${animation ? null : styles['pause']}
-        ${hover ? styles['orbit-hover'] : null}
+        ${hover || focus ? styles['orbit-hover'] : null}
       `}
+      style={ 
+        tanakaFocus
+        ? {zIndex: -1, transition: 'z-index 0s step-start'}
+        : tanakaView
+        ? {'visibility': 'collapse', transition: 'all 0.5s linear, visibility 0s step-start'} 
+        : {transition: 'all 0.5s linear, visibility 0.5s step-end'}
+      }
     >
       <section 
-        className={styles['minireddit']}
+        className={`
+          ${styles['minireddit']}
+          ${focus ? styles['minireddit-focus'] : null}
+          ${view ? styles['minireddit-view'] : null}
+        `}
         onMouseEnter = {handleHoverOn}
         onMouseLeave = {handleHoverOff}
+        onClick = {handleClick}
       >
       </section>
     </div>
