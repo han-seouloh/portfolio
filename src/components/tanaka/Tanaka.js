4372dde5b88c4a45de33b3c251bbc3a92de0c6a6
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-02-07 13:31:46 -0600

    Add click handler and update stylesheet

--- src/components/tanaka/Tanaka.js
+++ src/components/tanaka/Tanaka.js
@@ -1,12 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { selectPlanets, setPlanetAnimation, setPlanetOrbit } from '../../store/planetsSlice';
+import { selectClicks, setClickNum } from '../../store/clicksSlice';
+import { selectPlanets, setPlanetAnimation, setPlanetFocus, setPlanetOrbit, setPlanetView, setSelection } from '../../store/planetsSlice';
 import styles from './Tanaka.module.css';
 
 export const Tanaka = () => {
   const dispatch = useDispatch();
   const { planets } = useSelector(selectPlanets);
+  const { clicks } = useSelector(selectClicks);
+  const click = clicks.tanaka;
   const animation = planets.tanaka.animation;
   const hover = planets.tanaka.hover;
+  const focus = planets.tanaka.focus;
+  const view = planets.tanaka.view;
 
   const handleHoverOn = () => {
     dispatch(setPlanetAnimation(false));
@@ -16,19 +21,49 @@ export const Tanaka = () => {
     dispatch(setPlanetAnimation(true));
     dispatch(setPlanetOrbit({name:'tanaka', payload: false}));
   };
+  const handleClick = () => {
+    switch (click) {
+      case 0:
+        dispatch(setPlanetFocus({name:'tanaka', payload: true}));
+        dispatch(setPlanetAnimation(true));
+        dispatch(setSelection('tanaka'))
+        dispatch(setClickNum({name:'tanaka', clickNum: 1}));
+        break;
+      case 1:
+        dispatch(setPlanetFocus({name:'tanaka', payload: false}));
+        dispatch(setPlanetView({name:'tanaka', payload: true}));
+        dispatch(setPlanetAnimation(true));
+        dispatch(setClickNum({name:'tanaka', clickNum: 2}));
+        break;
+      case 2:
+        dispatch(setPlanetView({name:'tanaka', payload: false}));
+        dispatch(setPlanetAnimation(true));
+        dispatch(setPlanetOrbit({name:'tanaka', payload: false}));
+        dispatch(setClickNum({name:'tanaka', clickNum: 0}));
+        break;
+      default:
+        dispatch(setClickNum({name:'tanaka', clickNum: 0}));
+        break; 
+    };
+  };
 
   return (
     <div
       className={`
         ${styles['orbit']}
         ${animation ? null : styles['pause']}
-        ${hover ? styles['orbit-hover'] : null}
+        ${hover || focus ? styles['orbit-hover'] : null}
       `}
     >
       <section
-        className={styles['tanaka']}
+        className={`
+        ${styles['tanaka']}
+        ${focus ? styles['tanaka-focus'] : null}
+        ${view ? styles['tanaka-view'] : null}
+        `}
         onMouseEnter = {handleHoverOn}
         onMouseLeave = {handleHoverOff}
+        onClick = {handleClick}
       >
       </section>
     </div>
