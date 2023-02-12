d2325b7230d9a0907ae4fadcfe8d9c312344e274
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-02-11 23:55:44 -0600

    Refactor component and stylesheet and add Help component

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ import { selectPlanets} from './store/planetsSlice';
 /*Components*/
 import { About } from './components/about/About';
 import { Preview } from './components/preview/Preview';
+import { Help } from './components/help/Help';
 
 /*Styles*/
 import styles from './App.module.css';
 
 function App() {
-  const { planets } = useSelector(selectPlanets);
-  const focus = planets.about.focus;
+  const { planets, selection } = useSelector(selectPlanets);
+  const focus = planets[selection].focus;
 
   return (
-    <div className={styles.app}>
+    <div
+      className={styles.app}
+    >
       <div className={`${focus ? styles['planets-div-focus'] : styles['planets-div']}`}>
         <About />
       </div>
       <div className={`${focus ? styles['info-div-focus'] : styles['info-div']}`}>
         <Preview />
       </div>
+      <Help />
     </div>
   );
 }
