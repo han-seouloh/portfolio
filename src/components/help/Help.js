a97ce1ffe2413615fe6310e4fc34d0883c55a4d8
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-02-12 19:27:53 -0600

    Update component and stylesheet

--- src/components/help/Help.js
+++ src/components/help/Help.js
@@ -1,24 +1,28 @@
 import { useEffect, useState } from 'react';
-import { GrClose } from 'react-icons/gr'
-import { useSelector } from 'react-redux';
-import { selectPlanets } from '../../store/planetsSlice';
+import { CgClose } from 'react-icons/cg';
+import { useDispatch, useSelector } from 'react-redux';
+import { resetClicks } from '../../store/clicksSlice';
+import { resetPlanets, selectPlanets } from '../../store/planetsSlice';
 import styles from './Help.module.css';
 
 export const Help = () => {
   const [focus, setFocus] = useState(false);
   const { planets, selection } = useSelector(selectPlanets);
   const otherViews = planets[selection].view;
+  const dispatch = useDispatch();
   
   useEffect(() => {
     setFocus(false);
   }, [selection, otherViews]);
 
   const handleClick = () => {
+    dispatch(resetPlanets());
+    dispatch(resetClicks());
     if (focus) {
-      setFocus(false)
+      setFocus(false);
     } else {
-      setFocus(true)
-    }
+      setFocus(true);
+    };
   };
 
   return (
@@ -29,9 +33,26 @@ export const Help = () => {
       `}
       onClick={handleClick}
     >
-      <h2 className={focus ? null : styles.h2}>?</h2>
-      {focus && <GrClose />}
-      <section>
+      <h2 className={focus ? styles.disable : styles.h2}>?</h2>
+      {focus && <CgClose className={styles.close}/>}
+      <section className={focus ? styles.section : styles['section-off']}>
+        <h2>Tutorial</h2>
+        <p>Interactions with planets:</p>
+        <ul >
+          <li >
+            Hover on any of the orbiting planets to pause
+            the system and highlight its orbit. <span>(For Desktop Only)</span>
+          </li>
+          <li>
+            Click on any of the planets to focus it and
+            display a preview of the planet's content.
+            <span> (You can switch to other planets in this mode)</span>
+          </li>
+          <li>
+            While focused, a second click will display the contents
+            of the planet.
+          </li>
+        </ul>
       </section>
     </div>
   );
