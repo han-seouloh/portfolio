849bd031ec5f319ed8a664b45becefda2dc50ed9
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-02-21 13:10:45 -0600

    Add section components

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -3,6 +3,12 @@ import { CgClose } from 'react-icons/cg';
 import { resetPlanets, selectPlanets } from '../../store/planetsSlice';
 import styles from './Content.module.css';
 import { resetClicks } from '../../store/clicksSlice';
+import {ASection} from './details/ASection';
+import {ESection} from './details/ESection';
+import {CSection} from './details/CSection';
+import {PSection} from './details/PSection';
+import {MSection} from './details/MSection';
+import {TSection} from './details/TSection';
 
 export const Content = () => {
   const dispatch = useDispatch();
@@ -18,7 +24,12 @@ export const Content = () => {
     <main
       className={view ? styles['main-content'] : styles['main-content-hidden']}
     >
-
+      {(view && selection === 'about') && <ASection />}
+      {(view && selection === 'experience') && <ESection />}
+      {(view && selection === 'projects') && <  PSection />}
+      {(view && selection === 'current') && <CSection />}
+      {(view && selection === 'minireddit') && <MSection />}
+      {(view && selection === 'tanaka') && <TSection />}
       <CgClose
         className={styles.close}
         onClick={handleClick}
