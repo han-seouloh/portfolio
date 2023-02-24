d1440ef10494b996bac567cd3a39afb510d25870
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-02-23 19:30:11 -0600

    Develop experience section component

--- src/components/content/details/ESection.js
+++ src/components/content/details/ESection.js
@@ -1,16 +1,97 @@
+import { SiBootstrap, SiCss3, SiExpress, SiHtml5, SiJavascript, SiJquery, SiNextdotjs, SiNodedotjs, SiPostgresql, SiReact, SiRedux} from 'react-icons/si';
 import styles from './ESection.module.css';
 
 export const ESection = () => {
   return (
-    <section>
+    <section className={styles.main}>
       <div className={styles.title}>
         <h2>EXPERIENCE</h2>
+        <h2>&</h2>
+        <h2>STACK</h2>
       </div>
       <div className={styles.details}>
-        <h3>Introduction</h3>
-        <p></p>
-        <h3>Subtitle 2</h3>
-        <p></p>
+        <div className={styles['content-limiter']}>
+          <div className={styles['details-section']}>
+            <h3>Stack</h3>
+            <div className={styles.icons}>
+              <SiHtml5 />
+              <SiCss3 />
+              <SiJavascript />
+              <SiReact />
+              <SiRedux />
+              <SiJquery />
+              <SiBootstrap />
+              <SiNodedotjs />
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna 
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+              ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              Duis aute irure dolor in reprehenderit in voluptate velit 
+              esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+              occaecat cupidatat non proident, sunt in culpa qui officia 
+              deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div className={styles['details-section']}>
+            <h3>Stack (Learning)</h3>
+            <div className={styles['icons-2']}>
+              <SiExpress />
+              <SiNextdotjs />
+              <SiPostgresql />
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna 
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+              ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              Duis aute irure dolor in reprehenderit in voluptate velit 
+              esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+              occaecat cupidatat non proident, sunt in culpa qui officia 
+              deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div className={styles['details-section']}>
+            <h3>Education</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna 
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+              ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              Duis aute irure dolor in reprehenderit in voluptate velit 
+              esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+              occaecat cupidatat non proident, sunt in culpa qui officia 
+              deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div className={styles['details-section']}>
+            <h3>Experience</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna 
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+              ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              Duis aute irure dolor in reprehenderit in voluptate velit 
+              esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+              occaecat cupidatat non proident, sunt in culpa qui officia 
+              deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div className={styles['details-section']}>
+            <h3>Other Notable Skills</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna 
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+              ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              Duis aute irure dolor in reprehenderit in voluptate velit 
+              esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+              occaecat cupidatat non proident, sunt in culpa qui officia 
+              deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
