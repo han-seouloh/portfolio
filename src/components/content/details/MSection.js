8531df372a0411030efa23311f7d44aca66d550f
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-02-25 19:05:22 -0600

    Finish minireddit template

--- src/components/content/details/MSection.js
+++ src/components/content/details/MSection.js
@@ -1,16 +1,69 @@
+import { SiReddit } from 'react-icons/si';
 import styles from './MSection.module.css';
 
 export const MSection = () => {
+
   return (
-    <section>
+    <section className={styles.main}>
       <div className={styles.title}>
-        <h2>MINIREDDIT</h2>
+          <div className={styles['icon-container']}>
+            <SiReddit />
+          </div>
+          <h2><span>MINI</span>reddit</h2>
       </div>
-      <div className={styles.details}>
-        <h3>Introduction</h3>
-        <p></p>
-        <h3>Subtitle 2</h3>
-        <p></p>
+      <div className={styles.details} >
+        <div className={styles.section}>
+          <h3>Description</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna 
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit 
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+            occaecat cupidatat non proident, sunt in culpa qui officia 
+            deserunt mollit anim id est laborum.
+          </p>  
+        </div>
+        <div className={styles.section}>
+          <h3>Description 2</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna 
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit 
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+            occaecat cupidatat non proident, sunt in culpa qui officia 
+            deserunt mollit anim id est laborum.
+          </p> 
+        </div>
+        <div className={styles.section}>
+          <h3>Description 3</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna 
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit 
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+            occaecat cupidatat non proident, sunt in culpa qui officia 
+            deserunt mollit anim id est laborum.
+          </p>   
+        </div>
+        <div className={styles.section}>
+          <h3>Description 4</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna 
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit 
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+            occaecat cupidatat non proident, sunt in culpa qui officia 
+            deserunt mollit anim id est laborum.
+          </p>   
+        </div>
       </div>
     </section>
   );
