f581caf3b75402eb59ced17e135be9171f025407
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-02-25 19:27:52 -0600

    Finish projects section template

--- src/components/content/details/PSection.js
+++ src/components/content/details/PSection.js
@@ -1,16 +1,65 @@
 import styles from './PSection.module.css';
 
 export const PSection = () => {
+
   return (
-    <section>
+    <section className={styles.main}>
       <div className={styles.title}>
         <h2>PROJECTS</h2>
       </div>
-      <div className={styles.details}>
-        <h3>Introduction</h3>
-        <p></p>
-        <h3>Subtitle 2</h3>
-        <p></p>
+      <div className={styles.details} >
+        <div className={styles.section}>
+          <h3>Description</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna 
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit 
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+            occaecat cupidatat non proident, sunt in culpa qui officia 
+            deserunt mollit anim id est laborum.
+          </p>  
+        </div>
+        <div className={styles.section}>
+          <h3>Description 2</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna 
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit 
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+            occaecat cupidatat non proident, sunt in culpa qui officia 
+            deserunt mollit anim id est laborum.
+          </p> 
+        </div>
+        <div className={styles.section}>
+          <h3>Description 3</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna 
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit 
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+            occaecat cupidatat non proident, sunt in culpa qui officia 
+            deserunt mollit anim id est laborum.
+          </p>   
+        </div>
+        <div className={styles.section}>
+          <h3>Description 4</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna 
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit 
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+            occaecat cupidatat non proident, sunt in culpa qui officia 
+            deserunt mollit anim id est laborum.
+          </p>   
+        </div>
       </div>
     </section>
   );
