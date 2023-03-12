f9389f9f180b9df7a91bac44f8a05df7e594faeb
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-03-11 20:36:28 -0600

    Add scrollSlice

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import planetsReducer from "./planetsSlice";
 import infoReducer from "./infoSlice";
-import clicksReducer from "./clicksSlice";
 import sectionsReducer from "./sectionsSlice";
+import clicksReducer from "./clicksSlice";
+import scrollReducer from "./scrollSlice";
 
 export default configureStore ({
   reducer: {
     planets: planetsReducer,
     info: infoReducer,
+    sections: sectionsReducer,
     clicks: clicksReducer,
-    sections: sectionsReducer
+    scroll: scrollReducer
   }
 });
