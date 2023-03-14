1582c13bcae2b6a360486fc0ab47644b4c14063f
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-03-13 19:22:00 -0600

    Update component to match other sections

--- src/components/content/details/ESection.js
+++ src/components/content/details/ESection.js
@@ -1,7 +1,44 @@
-import { SiBootstrap, SiCss3, SiExpress, SiHtml5, SiJavascript, SiJquery, SiNextdotjs, SiNodedotjs, SiPostgresql, SiReact, SiRedux} from 'react-icons/si';
+import {ScrollDown} from '../../scrolldown/ScrollDown';
+import { SiBootstrap, SiCss3, SiExpress, SiHtml5, SiJavascript, SiJquery, SiNextdotjs, SiNodedotjs, SiPostgresql, SiPython, SiReact, SiRedux, SiRust, SiSass} from 'react-icons/si';
 import styles from './ESection.module.css';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { resetScroll, setScroll } from '../../../store/scrollSlice';
 
 export const ESection = () => {
+  const dispatch = useDispatch();
+  const [scrollY, setScrollY] = useState(0);
+  const [direction, setDirection] = useState('');
+  const portrait = window.matchMedia("(orientation: portrait)").matches;
+
+  const handleScroll = ( {target} ) => {
+    const newScroll = target.scrollTop;
+
+    if (scrollY > newScroll) {
+      setDirection('up');
+      setScrollY(newScroll);
+    } else {
+      setDirection('down');
+      setScrollY(newScroll);
+    };
+
+    if (newScroll === 0) {
+      dispatch(resetScroll());
+    } else {
+      dispatch(setScroll());
+    }
+
+    if (portrait) {
+      if (direction === 'down') {
+        target.parentElement.children[0].classList.add(styles['title-scroll']);
+        target.classList.add(styles['details-scroll']);
+      } else {
+        target.parentElement.children[0].classList.remove(styles['title-scroll']);
+        target.classList.remove(styles['details-scroll']);
+      }
+    };
+  };
+
   return (
     <section className={styles.main}>
       <div className={styles.title}>
@@ -9,9 +46,8 @@ export const ESection = () => {
         <h2>&</h2>
         <h2>STACK</h2>
       </div>
-      <div className={styles.details}>
-        <div className={styles['content-limiter']}>
-          <div className={styles['details-section']}>
+      <div className={styles.details} onScroll={handleScroll}>
+          <div className={styles.section}>
             <h3>Stack</h3>
             <div className={styles.icons}>
               <SiHtml5 />
@@ -22,6 +58,8 @@ export const ESection = () => {
               <SiJquery />
               <SiBootstrap />
               <SiNodedotjs />
+              <SiSass />
+              <SiPython />
             </div>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
@@ -33,13 +71,15 @@ export const ESection = () => {
               occaecat cupidatat non proident, sunt in culpa qui officia 
               deserunt mollit anim id est laborum.
             </p>
+            <ScrollDown />
           </div>
-          <div className={styles['details-section']}>
+          <div className={styles.section}>
             <h3>Stack (Learning)</h3>
             <div className={styles['icons-2']}>
               <SiExpress />
               <SiNextdotjs />
               <SiPostgresql />
+              <SiRust />
             </div>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
@@ -52,7 +92,7 @@ export const ESection = () => {
               deserunt mollit anim id est laborum.
             </p>
           </div>
-          <div className={styles['details-section']}>
+          <div className={styles.section}>
             <h3>Education</h3>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
@@ -65,7 +105,7 @@ export const ESection = () => {
               deserunt mollit anim id est laborum.
             </p>
           </div>
-          <div className={styles['details-section']}>
+          <div className={styles.section}>
             <h3>Experience</h3>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
@@ -78,7 +118,7 @@ export const ESection = () => {
               deserunt mollit anim id est laborum.
             </p>
           </div>
-          <div className={styles['details-section']}>
+          <div className={styles.section}>
             <h3>Other Notable Skills</h3>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
@@ -91,7 +131,6 @@ export const ESection = () => {
               deserunt mollit anim id est laborum.
             </p>
           </div>
-        </div>
       </div>
     </section>
   );
