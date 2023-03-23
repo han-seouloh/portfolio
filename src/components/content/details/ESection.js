f706c89c3b1af788cf5c566ead4eb19e6c930d2d
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-03-22 21:03:49 -0600

    Add hover feature on icons

--- src/components/content/details/ESection.js
+++ src/components/content/details/ESection.js
@@ -1,5 +1,5 @@
 import {ScrollDown} from '../../scrolldown/ScrollDown';
-import { SiBootstrap, SiCss3, SiExpress, SiHtml5, SiJavascript, SiJquery, SiNextdotjs, SiNodedotjs, SiPostgresql, SiPython, SiReact, SiRedux, SiRust, SiSass} from 'react-icons/si';
+import { SiBootstrap, SiCss3, SiExpress, SiHtml5, SiJavascript, SiJquery, SiNodedotjs, SiPostgresql, SiPython, SiReact, SiRedux, SiSass} from 'react-icons/si';
 import styles from './ESection.module.css';
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
@@ -10,6 +10,7 @@ export const ESection = () => {
   const [scrollY, setScrollY] = useState(0);
   const [direction, setDirection] = useState('');
   const portrait = window.matchMedia("(orientation: portrait)").matches;
+  let iconName = document.getElementById('icon-name');
 
   const handleScroll = ( {target} ) => {
     const newScroll = target.scrollTop;
@@ -32,12 +33,48 @@ export const ESection = () => {
       if (direction === 'down') {
         target.parentElement.children[0].classList.add(styles['title-scroll']);
         target.classList.add(styles['details-scroll']);
+        iconName.innerHTML = '';
       } else {
         target.parentElement.children[0].classList.remove(styles['title-scroll']);
         target.classList.remove(styles['details-scroll']);
+        iconName.innerHTML = '';
       }
     };
   };
+
+  const handleHTML = ( ) => {
+    iconName.innerHTML = 'HTML';
+  };
+  const handleCSS = ( ) => {
+    iconName.innerHTML = 'CSS';
+  };
+  const handleJavascript = ( ) => {
+    iconName.innerHTML = 'JavaScript';
+  };
+  const handleReact = ( ) => {
+    iconName.innerHTML = 'React';
+  };
+  const handleRedux = ( ) => {
+    iconName.innerHTML = 'Redux';
+  };
+  const handleJquery = ( ) => {
+    iconName.innerHTML = 'JQuery';
+  };
+  const handleBoot = ( ) => {
+    iconName.innerHTML = 'Bootstrap';
+  };
+  const handleNode = ( ) => {
+    iconName.innerHTML = 'Node';
+  };
+  const handleSass = ( ) => {
+    iconName.innerHTML = 'SASS';
+  };
+  const handlePython = ( ) => {
+    iconName.innerHTML = 'Python';
+  };
+  const handleHide = () => {
+    iconName.innerHTML = '';
+  }
 
   return (
     <section className={styles.main}>
@@ -48,61 +85,59 @@ export const ESection = () => {
       <div className={styles.details} onScroll={handleScroll}>
           <div className={styles.section}>
             <h3>Stack</h3>
-            <div className={styles.icons}>
-              <SiHtml5 />
-              <SiCss3 />
-              <SiJavascript />
-              <SiReact />
-              <SiRedux />
-              <SiJquery />
-              <SiBootstrap />
-              <SiNodedotjs />
-              <SiSass />
-              <SiPython />
+            <div className={styles.icons} onMouseLeave={handleHide}>
+              <SiHtml5 className={styles.icon} onMouseEnter={handleHTML}/>
+              <SiCss3 className={styles.icon} onMouseEnter={handleCSS}/>
+              <SiJavascript className={styles.icon} onMouseEnter={handleJavascript}/>
+              <SiReact className={styles.icon} onMouseEnter={handleReact}/>
+              <SiRedux className={styles.icon} onMouseEnter={handleRedux}/>
+              <SiJquery className={styles.icon} onMouseEnter={handleJquery}/>
+              <SiBootstrap className={styles.icon} onMouseEnter={handleBoot}/>
+              <SiNodedotjs className={styles.icon} onMouseEnter={handleNode}/>
+              <SiSass className={styles.icon} onMouseEnter={handleSass}/>
+              <SiPython className={styles.icon} onMouseEnter={handlePython}/>
             </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed do eiusmod tempor incididunt ut labore et dolore magna 
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-              ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-              Duis aute irure dolor in reprehenderit in voluptate velit 
-              esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-              occaecat cupidatat non proident, sunt in culpa qui officia 
-              deserunt mollit anim id est laborum.
-            </p>
+            <p id='icon-name' className={styles['icon-name']}></p>
             <ScrollDown />
           </div>
           <div className={styles.section}>
             <h3>Stack (Learning)</h3>
             <div className={styles['icons-2']}>
               <SiExpress />
-              <SiNextdotjs />
               <SiPostgresql />
-              <SiRust />
             </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed do eiusmod tempor incididunt ut labore et dolore magna 
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-              ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-              Duis aute irure dolor in reprehenderit in voluptate velit 
-              esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-              occaecat cupidatat non proident, sunt in culpa qui officia 
-              deserunt mollit anim id est laborum.
-            </p>
+            <ul className={styles.list}>
+              <p className={styles.brief}>
+                These are the languages, frameworks, libraries, and technologies I'm currently 
+                learning to become a Full-Stack developer. In the future, I plan to learn MongoDB 
+                and ThreeJS to further expand my backend development skills and increase my frontend 
+                development arsenal.
+              </p>
+              <li>
+                <h4>ExpressJS</h4>
+              </li>
+              <li>
+                <h4>PostgreSQL</h4>
+              </li>
+            </ul>
           </div>
           <div className={styles.section}>
             <h3>Education</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed do eiusmod tempor incididunt ut labore et dolore magna 
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-              ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-              Duis aute irure dolor in reprehenderit in voluptate velit 
-              esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-              occaecat cupidatat non proident, sunt in culpa qui officia 
-              deserunt mollit anim id est laborum.
-            </p>
+            <ul className={styles.list}>
+              <li>
+                <h4>BS in Mechanical Engineering with Minor in Industrial Engineering</h4>
+                <p>Tecnológico de Monterrey (ITESM)</p>
+                <p>Graduated with Honors</p>
+              </li>
+              <li>
+                <h4>Something</h4>
+                <p></p>
+              </li>
+              <li>
+                <h4>Something</h4>
+                <p></p>
+              </li>
+            </ul>
           </div>
           <div className={styles.section}>
             <h3>Experience</h3>
